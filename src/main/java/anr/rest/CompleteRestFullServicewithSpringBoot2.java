4b3e2b8e3f1c1c9aa034034ba76d8755cc25a61c
Ashwarth <aswarthaanr@gmail.com>
2019-08-17 19:56:49 +0530

    First commit

---
+++ src/main/java/anr/rest/CompleteRestFullServicewithSpringBoot2.java
@@ -0,0 +1,13 @@
+package anr.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteRestFullServicewithSpringBoot2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteRestFullServicewithSpringBoot2.class, args);
+	}
+
+}
